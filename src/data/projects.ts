d5626f061acbeb371638133f66b7c8c0a0edeb4f
Bashamega <adambashaahmednaji@gmail.com>
2024-08-17 12:13:00 +0300

    Integration

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,21 +1,15 @@
-const projects = [
-  {
-    url: 'ZemPosts',
-    id: '1',
-    banner: '/projects/ZemPosts/banner.png',
-    img: '/projects/ZemPosts/logo.png',
-    title: 'ZemPosts',
-    type: 'Astro, ReactJS, Typescript, CSS',
-    icon: '/projects/ZemPosts/logo.png',
-    blog: 'https://zemerik.hashnode.dev/introducing-zemposts',
-    github: 'https://github.com/Zemerik/ZemPosts',
-    web: 'https://zemposts.vercel.app',
-    description:
-      "ZemPosts stands as an open-source platform, providing users with the opportunity to write and read blogs seamlessly. This connectivity is fostered through the simple act of publishing one's blog to the platform, thus creating a digital hub where like-minded individuals can engage, collaborate, and share insights within the expansive realm of writing and blogging. With its user-friendly interface and inclusive community ethos, ZemPosts serves as a dynamic space where connections are forged, ideas are exchanged, and creativity thrives.",
-    tags: [
-      {name: 'Astro, ReactJS, Typescript, CSS'},
-    ],
-  },
-]
+import { Project } from "../types/Project";
+import prisma from "../utils/db";
 
-export default projects
+
+export async function getProjects(){
+  try {
+    // Fetching projects from the database
+    const projects = await prisma.project.findMany();
+    return projects;
+  } catch (error) {
+    // Logging and rethrowing the error for further handling
+    console.error("Error retrieving projects:", error);
+    return []
+  }
+}
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import projects from "../data/projects";
+import { useEffect, useState } from "react";
+import { getProjects } from "../data/projects";
 import { Footer } from "../components/Footer";
 import { ScrollTop } from "../components/ScrollTop";
 import * as S from "../styles/projects";
@@ -10,22 +10,29 @@ import * as T from "../styles/styles";
 import { HiOutlineDesktopComputer } from "react-icons/hi";
 import { FaSearch } from "react-icons/fa";
 import { ArrowRight } from "phosphor-react";
+import { Project } from "../types/Project";
 
 const botkey = process.env.NEXT_PUBLIC_BOTKEY_URL;
 
-interface ProjectsProps {
-  target: HTMLInputElement;
-}
-
 export default function Projects() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState<string>("");
+  const [projects, setProjects] = useState<Project[] | any>([]);
 
-  const handleChange = (e: ProjectsProps) => {
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const projectsData = await getProjects();
+      setProjects(projectsData);
+    };
+
+    fetchProjects();
+  }, []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
   };
 
-  const projectFilter = projects.filter((project) =>
-    project.title.toLowerCase().includes(query.toLowerCase())
+  const filteredProjects = projects.filter((project:any) =>
+    project.title?.toLowerCase().includes(query.toLowerCase())
   );
 
   return (
@@ -53,9 +60,7 @@ export default function Projects() {
             <HiOutlineDesktopComputer />
           </span>
         </T.Title>
-        <T.Description>
-          Showcase & Connect with Developers
-        </T.Description>
+        <T.Description>Showcase & Connect with Developers</T.Description>
 
         <S.ProjectsContainer>
           <S.ProjectsContent>
@@ -73,61 +78,50 @@ export default function Projects() {
               </div>
             </div>
 
-            {!projectFilter.length && (
+            {filteredProjects.length === 0 && (
               <h3 className="not-found">Project not found 🙁</h3>
             )}
 
-            {projectFilter.map((project) => {
-              return (
-                <>
-                  <div className="border" key={project.id} />
-                  <S.ProjectsItem>
-                    <div className="banner">
-                      <Image
-                        style={{ borderRadius: "20px" }}
-                        width={500}
-                        height={300}
-                        src={project.img}
-                        alt={project.title}
+            {filteredProjects.map((project:Project) => (
+              <S.ProjectsItem key={project.id}>
+                <div className="banner">
+                  <Image
+                    style={{ borderRadius: "20px" }}
+                    width={500}
+                    height={300}
+                    src={project.imgUrl}
+                    alt={project.title}
+                  />
+                </div>
+
+                <div>
+                  <div className="title">
+                    <h2>{project.title}</h2>
+                  </div>
+                  <div className="description">
+                    <p>{project.description}</p>
+                    <div className="tags"></div>
+                  </div>
+                  <Link href={`/project/${project.url}`}>
+                    <T.ButtonAlternatives>
+                      View Project
+                      <ArrowRight
+                        style={{
+                          marginBottom: "-0.1rem",
+                        }}
+                        weight="bold"
+                        size={16}
                       />
-                    </div>
-
-                    <div>
-                      <div className="title">
-                        <h2>{project.title}</h2>
-                      </div>
-                      <div className="description">
-                        <p>{project.description}</p>
-                        <div className="tags">
-                        </div>
-                      </div>
-                      <Link href={`/project/${project.url}`}>
-                        <T.ButtonAlternatives>
-                          View Project
-                          <ArrowRight
-                            style={{
-                              marginBottom: "-0.1rem",
-                            }}
-                            weight="bold"
-                            size={16}
-                          />
-                        </T.ButtonAlternatives>
-                      </Link>
-                    </div>
-                  </S.ProjectsItem>
-                </>
-              );
-            })}
-
-            
+                    </T.ButtonAlternatives>
+                  </Link>
+                </div>
+              </S.ProjectsItem>
+            ))}
           </S.ProjectsContent>
         </S.ProjectsContainer>
       </T.Section>
       <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
-      <script
-        src={botkey}
-        defer
-      ></script>
+      <script src={botkey} defer></script>
       <Footer />
     </>
   );
